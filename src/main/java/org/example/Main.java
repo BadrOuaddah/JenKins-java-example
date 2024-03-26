8deddd64ddfc5e080044202f6edf53f83746c79b
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-03-26 17:59:41 +0000

    Print result of addition

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Friend!");
+
+        Calculator calculator = new Calculator();
+
+        float addition = calculator.addition(5F, 5F);
+
+        System.out.println(addition);
     }
 }
